6ac795805d776a21f8c5d718888cf18ff4e3f212
thiagosaraiva-git <saraii_@live.com>
2023-10-14 12:57:04 -0300

    feat: cart modal add

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Badge } from '@mui/material';
-import { ShoppingCart, Favorite } from '@mui/icons-material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Badge, Modal, Box, List, ListItem, ListItemText } from '@mui/material';
+import { ShoppingCart } from '@mui/icons-material';
 
 import { useAtom } from 'jotai';
 import { cartAtom } from '@/store/cart';
 
+import './Header.scss'
+
 const Header = () => {
     const [cart] = useAtom(cartAtom);
+    const [open, setOpen] = useState(false);
+
+    const handleCart = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <AppBar position="static">
             <Toolbar>
@@ -16,13 +28,24 @@ const Header = () => {
                     My Store
                 </Typography>
                 <IconButton color="inherit">
-                    <Badge badgeContent={cart.length} color="secondary">
+                    <Badge badgeContent={cart.length} color="secondary" onClick={handleCart}>
                         <ShoppingCart />
                     </Badge>
                 </IconButton>
-                <IconButton color="inherit">
-                    <Favorite />
-                </IconButton>
+                <Modal open={open} onClose={handleClose}>
+                    <Box className="cartModal">
+                        <Typography variant="h6" component="h2" gutterBottom>
+                            Cart Items
+                        </Typography>
+                        <List>
+                            {cart.map((item, index) => (
+                                <ListItem key={index}>
+                                    <ListItemText primary={item.name} secondary={`Quantity: ${item.quantity}`} />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Box>
+                </Modal>
             </Toolbar>
         </AppBar>
     );
